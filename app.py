8faaac24bfea8dfa241bb9b105246b4ed08b67d5
renusree153 <89613550+renusree153@users.noreply.github.com>
2022-10-22 16:52:09 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/')
+def home ():
+  return render_template('/src/Userinfo.js')
+  
+  
+@app.route('/predict', methods =['POST'])
+def predict():
+  int_features = [float(x) for x in request.form.values()]
+  features = [np.array(int_features)]
+  prediction = model.predict(features)
+  
+  output = round(prediction[0], 2)
+  return render_template ('Userinfo.js', prediction_text = 'Possibility for diabetes is {}'.format(output))
+  
+  if __name__ == "__main__":
+    app.run()
+    
